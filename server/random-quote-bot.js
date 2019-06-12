647c3bf268d8c360de470827decfae0220b6255c
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-06-12 10:56:05 +0530

    Added random quote bot

---
+++ server/random-quote-bot.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var Twitter = require('twitter');
+var config = require('./config.js');
+
+var T = new Twitter(config);
+
+// Set up your search parameters
+var params = {
+  q: '#nodejs, #100DaysOfCode, #301DaysOfCode, #JavaScript, #ReactJS',
+  count: 5,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&key=123456&format=text&lang=en';
+
+function getQuote(callback) {
+  request(url, function (error, response, body) {
+    console.log("error:", error); // Print the error if one occurred
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    console.log("body:", body);
+    callback(body);
+  });
+}
+
+function postTweet(tweet) {
+  console.log(tweet);
+  T.post("statuses/update", { status: tweet }, function (err, data, response) {
+    console.log(data);
+  });
+}
+
+getQuote(postTweet);
